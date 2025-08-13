af06c7f344cdc0d1aed7129afcd21111b132656c
Jonah Cheadle <jonahcheadle04@gmail.com>
2025-08-13 17:47:28 -0500

    added Home

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,12 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import "./Footer.css";
 
 const Footer = () => {
+  const handleHomeClick = () => {
+    window.scrollTo(0, 0);
+  };
+
   return (
     <footer className="footer">
       <p>© 2025 Christy's Lagniappe Music. All rights reserved.</p>
@@ -9,6 +14,9 @@ const Footer = () => {
       <a href="#top" className="back-to-top">
         ↑ Back to Top
       </a>
+      <Link to="/" onClick={handleHomeClick} className="home">
+        Home
+      </Link>
     </footer>
   );
 };
